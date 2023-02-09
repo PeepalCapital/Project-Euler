80e7def163be8783b8f8a790bfd93c4036dd307a
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2023-02-08 21:50:38 -0800

    Create 40_Euler.py

---
+++ 40_Euler.py
@@ -0,0 +1,2 @@
+#Champernowne's constant related problem doesn't require any code to solve. One can solve it with pen and paper in 5 minutes or less.
+#Not sure why this problem finds a mention in Project Euler
