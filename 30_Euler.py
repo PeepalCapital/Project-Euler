d7c1f50c0296e0e3d2774894f8d258d3588d49f5
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2023-02-08 19:30:29 -0800

    Update 30_Euler.py

--- 30_Euler.py
+++ 30_Euler.py
@@ -2,7 +2,23 @@
 
 #Project Euler Problem 30
 
-#this is not the correct way to solve it. not at all a good way. but gets the job done
+#Time for execution: 27547.89400100708 ms
+
+#For each number of digits the maximum limit will be the number of digits * 9^5.
+#Max Limit - Lowest Number Possible - Highest Number Possible - (Highest - Max Limit)
+#max limit	lowest	highest	
+#59049	0	9	-59040
+#118098	10	99	-117999
+#177147	100	999	-176148
+#236196	1000	9999	-226197
+#295245	10000	99999	-195246
+#354294	100000	999999	645705
+#413343	1000000	9999999	9586656
+#472392	10000000	99999999	99527607
+#531441	100000000	999999999	999468558
+
+#so we can check up to 999,999 as the number for the power of 5
+
 
 def n_power_digit_sum(n,p):
     string_n = str(n)
@@ -17,4 +33,5 @@ for i in range (0, 10000000):
     if i == term:
         list_trial.append(i)
     
-print(list_trial)
+print(list_trial) 
+print(sum(list_trial) - 1)
